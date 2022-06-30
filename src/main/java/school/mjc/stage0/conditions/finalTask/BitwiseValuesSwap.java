772bf92f82162df311cd87d95afcc5644530da75
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-30 21:15:03 +0400

    first_try

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public static void swap(int first, int second) {
+    public void swap(int first, int second) {
         first = first ^ second;
         second = first ^ second;
         first = first ^ second;
